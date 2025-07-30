03a5a45826c0fa23a4ca8ef786f44b30897866fd
phanhai78 <bmdaoconghg@gmail.com>
2025-07-30 04:15:50 -0400

    second commit

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -23,6 +23,7 @@ BASE_URL = "/accounts"
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -38,7 +39,15 @@ class TestAccountService(TestCase):
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
-
+        def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
     def setUp(self):
         """Runs before each test"""
         db.session.query(Account).delete()  # clean up the last tests
@@ -69,11 +78,29 @@ class TestAccountService(TestCase):
             account.id = new_account["id"]
             accounts.append(account)
         return accounts
+        ######################################################################
+    # READ AN ACCOUNT
+    ######################################################################
+    @app.route("/accounts/<int:account_id>", methods=["GET"])
+    def get_accounts(account_id):
+        """
+        Reads an Account
+        This endpoint will read an Account based the account_id that is requested
+        """
+        app.logger.info("Request to read an Account with id: %s", account_id)
 
+        account = Account.find(account_id)
+        if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+        return account.serialize(), status.HTTP_200_OK
     ######################################################################
     #  A C C O U N T   T E S T   C A S E S
     ######################################################################
-
+        def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        resp = self.client.get(f"{BASE_URL}/0")
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
     def test_index(self):
         """It should get 200_OK from the Home Page"""
         response = self.client.get("/")
@@ -107,7 +134,22 @@ class TestAccountService(TestCase):
         self.assertEqual(new_account["address"], account.address)
         self.assertEqual(new_account["phone_number"], account.phone_number)
         self.assertEqual(new_account["date_joined"], str(account.date_joined))
+        ######################################################################
+    # READ AN ACCOUNT
+    ######################################################################
+    @app.route("/accounts/<int:account_id>", methods=["GET"])
+    def get_accounts(account_id):
+        """
+        Reads an Account
+        This endpoint will read an Account based the account_id that is requested
+        """
+        app.logger.info("Request to read an Account with id: %s", account_id)
 
+        account = Account.find(account_id)
+        if not account:
+            abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
+        return account.serialize(), status.HTTP_200_OK
     def test_bad_request(self):
         """It should not Create an Account when sending the wrong data"""
         response = self.client.post(BASE_URL, json={"name": "not enough data"})
